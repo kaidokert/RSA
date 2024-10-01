9c1bec8a9676355e47a9ed4282a79ca936947167
Kaido Kert <kaidokert@gmail.com>
2024-09-30 18:59:32 -0700

    PSS verification

--- src/algorithms/pss.rs
+++ src/algorithms/pss.rs
@@ -58,6 +58,8 @@ fn emsa_pss_verify_pre<'a>(
 
     // 4. If the rightmost octet of EM does not have hexadecimal value
     //    0xbc, output "inconsistent" and stop.
+    #[cfg(feature = "std")]
+    println!("em[last]: {:x?}", em[em.len() - 1]);
     if em[em.len() - 1] != 0xBC {
         return Err(Error::Verification);
     }
@@ -67,11 +69,37 @@ fn emsa_pss_verify_pre<'a>(
     let (db, h) = em.split_at_mut(em_len - h_len - 1);
     let h = &mut h[..h_len];
 
-    todo!()
+    #[cfg(feature = "std")]
+    println!("db: {:x?}", db);
+    #[cfg(feature = "std")]
+    println!("h: {:x?}", h);
+    #[cfg(feature = "std")]
+    println!("em_len: {} em_bits {}", em_len, em_bits);
+
+    // 6. If the leftmost 8 * em_len - em_bits bits of the leftmost octet in
+    //    maskedDB are not all equal to zero, output "inconsistent" and
+    //    stop.
+    if db[0]
+        & (0xFF_u8
+            .checked_shl(8 - (8 * em_len - em_bits) as u32)
+            .unwrap_or(0))
+        != 0
+    {
+        #[cfg(feature = "std")]
+        println!("Verification error");
+        return Err(Error::Verification);
+    }
+
+    Ok((db, h))
 }
 
 fn emsa_pss_verify_salt(db: &[u8], em_len: usize, s_len: usize, h_len: usize) -> Choice {
-    todo!()
+    let (zeroes, rest) = db.split_at(em_len - h_len - s_len - 2);
+    let valid: Choice = zeroes
+        .iter()
+        .fold(Choice::from(1u8), |a, e| a & e.ct_eq(&0x00));
+
+    valid & rest[0].ct_eq(&0x01)
 }
 
 pub(crate) fn emsa_pss_verify(
@@ -111,5 +139,39 @@ where
 
     let (db, h) = emsa_pss_verify_pre(m_hash, em, em_bits, s_len, h_len)?;
 
-    todo!()
+    let mut hash = D::new();
+
+    // 7. Let dbMask = MGF(H, em_len - h_len - 1)
+    //
+    // 8. Let DB = maskedDB \xor dbMask
+    mgf1_xor_digest::<D>(db, &mut hash, &*h);
+
+    // 9.  Set the leftmost 8 * emLen - emBits bits of the leftmost octet in DB
+    //     to zero.
+    db[0] &= 0xFF >> /*uint*/(8 * em_len - em_bits);
+
+    let salt_valid = emsa_pss_verify_salt(db, em_len, s_len, h_len);
+
+    // 11. Let salt be the last s_len octets of DB.
+    let salt = &db[db.len() - s_len..];
+
+    // 12. Let
+    //          M' = (0x)00 00 00 00 00 00 00 00 || mHash || salt ;
+    //     M' is an octet string of length 8 + hLen + sLen with eight
+    //     initial zero octets.
+    //
+    // 13. Let H' = Hash(M'), an octet string of length hLen.
+    let prefix = [0u8; 8];
+
+    Digest::update(&mut hash, &prefix[..]);
+    Digest::update(&mut hash, m_hash);
+    Digest::update(&mut hash, salt);
+    let h0 = hash.finalize_reset();
+
+    // 14. If H = H', output "consistent." Otherwise, output "inconsistent."
+    if (salt_valid & h0.ct_eq(h)).into() {
+        Ok(())
+    } else {
+        Err(Error::Verification)
+    }
 }
