22b3708b6f85392cd4da81129356107f90cd217d
Kaido Kert <kaidokert@gmail.com>
2026-03-28 18:55:08 -0700

    Add nostd code

--- integrate_thumb/examples/hello.rs
+++ integrate_thumb/examples/hello.rs
@@ -1,9 +1,12 @@
 #![no_std]
 #![no_main]
 
+use cortex_m_semihosting::{debug, hprintln};
 use panic_semihosting as _;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
+    hprintln!("integrate_thumb hello");
+    debug::exit(debug::EXIT_SUCCESS);
     loop {}
 }
