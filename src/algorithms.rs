fb7769e72bb59fde96dafe3623a7a939d89d8cdd
Kaido Kert <kaidokert@gmail.com>
2026-03-28 18:18:50 -0700

    more cleanup

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -3,12 +3,12 @@
 #[cfg(feature = "full")]
 mod mgf;
 
-#[cfg(feature = "alloc")]
+#[cfg(feature = "private-key")]
 pub(crate) mod generate;
 #[cfg(feature = "full")]
 pub(crate) mod oaep;
 pub(crate) mod pad;
 pub(crate) mod pkcs1v15;
-#[cfg(feature = "alloc")]
+#[cfg(feature = "full")]
 pub(crate) mod pss;
 pub(crate) mod rsa;
