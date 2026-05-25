d3cba7e15a6499b7970c746ffee87a69c253e658
Kaido Kert <kaidokert@gmail.com>
2026-05-24 19:51:16 -0700

    Typo fix

--- src/oaep/encrypting_key.rs
+++ src/oaep/encrypting_key.rs
@@ -69,7 +69,7 @@ where
         }
     }
 
-    /// Create a new verifying key from an RSA public key using provided label.
+    /// Create a new encrypting key from an RSA public key using provided label.
     /// no_alloc counterpart that accepts a fixed-capacity [`Label`].
     #[cfg(not(feature = "alloc"))]
     pub fn new_with_label(key: GenericRsaPublicKey<T, M>, label: Label) -> Self {
